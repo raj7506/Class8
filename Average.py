335e2bb79b652f832f590610cc3a95381baa4684
raj7506 <krkrajan@gmail.com>
2025-10-04 14:27:30 +0530

    Update Average.py

--- Average.py
+++ Average.py
@@ -1,7 +1,7 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 num3 = int(input("Enter third number: "))
-avg = num1 + num2 + num3/3
+avg = (num1 + num2 + num3)/3
 if avg>num1 and avg>num2 and avg>num3:
     print("%d is higher %d,%d,%d"%(avg,num1, num2, num3))
 elif avg>num1 and avg>num2:
@@ -17,4 +17,5 @@ elif avg>num2:
 elif avg>num3:
     print("%d is just higher than %d"%(avg,num3))
 else:
+
     print("Invalid input")
