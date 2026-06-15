18499e0a97a5d7385ee637974ac9736be9726db9
raj7506 <krkrajan@gmail.com>
2026-06-15 11:30:51 +0530

    Add files via upload

---
+++ number1.py
@@ -0,0 +1,15 @@
+def getmaxlength(a,a_size):
+    counter = 0
+    maxones = 1
+    for i in range(0, a_size):
+        if(a[i] == 0):
+            counter = 0
+        else:
+            counter += 1
+            maxones = max(maxones, counter)
+    return maxones
+
+a = [1,1,1,0,0,1,0,1,1,1,1]
+a_size = len(a)
+
+print("Max 1: ", getmaxlength(a,a_size))
